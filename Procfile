c392b15ad4ccad92b0db71fdf8880f2ecf33a775
ThakorRohan <67776672+ThakorRohan@users.noreply.github.com>
2020-07-03 14:09:48 +0000

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: python3 -m spiderman
+worker: python3 -m spiderman
+ps:scale worker=4
---
+++ spiderman/modules/helper_funcs/admin_rights.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from telegram import (
+          User, 
+          Chat, 
+          ChatMember, 
+          Update, 
+          Bot)
+
+def user_can_promote(chat: Chat, user: User, bot_id: int) -> bool:
+	return chat.get_member(user.id).can_promote_members
+	
+def user_can_ban(chat: Chat, user: User, bot_id: int) -> bool:
+	return chat.get_member(user.id).can_restrict_members
+	
+def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
+	return chat.get_member(user.id).can_pin_messages
+	
+def user_can_delete(chat: Chat, user: User, bot_id: int) -> bool:
+	return chat.get_member(user.id).can_delete_messages
